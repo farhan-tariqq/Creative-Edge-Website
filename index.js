8bce7d7f3b117f5727f80f59b1ec4fdce9974a4e
Farhan Tariq <121147549+farhan-tariqq@users.noreply.github.com>
2023-07-23 22:44:58 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,68 @@
+function sendMail() {
+  var params = {
+    name: document.getElementById("name").value,
+    // email: document.getElementById("email").value,
+    number: document.getElementById("number").value,
+    message: document.getElementById("message").value,
+  };
+
+  const serviceID = "service_j76zquy";
+  const templateID = "template_qzarupv";
+
+   /*  emailjs.send(serviceID, templateID, params)
+    .then(res=>{
+        document.getElementById("name").value = "";
+        document.getElementById("email").value = "";
+        document.getElementById("number").value = "";
+       // document.getElementById("file").value = "";
+        document.getElementById("message").value = "";
+        // var message = document.getElementById("message").value;
+
+        console.log(res);
+        alert('Your message sent successfully!!')
+
+    })
+ */
+    emailjs.send(serviceID, templateID, params)
+    .then(res => {
+      document.getElementById("name").value = "";
+      // document.getElementById("email").value = "";
+      document.getElementById("number").value = "";
+      // document.getElementById("file").value = "";
+      document.getElementById("message").value = "";
+      // var message = document.getElementById("message").value;
+  
+      console.log(res);
+  
+      // Create a centered alert div element
+      var alertDiv = document.createElement("div");
+      alertDiv.id = "centered-alert";
+      alertDiv.style.position = "fixed";
+      alertDiv.style.top = "50%";
+      alertDiv.style.left = "50%";
+      alertDiv.style.transform = "translate(-50%, -50%)";
+      alertDiv.style.backgroundColor = "lightblue";
+      alertDiv.style.padding = "10px";
+      alertDiv.style.borderRadius = "5px";
+      alertDiv.style.boxShadow = "0 2px 4px rgba(0, 0, 0, 0.5)";
+      alertDiv.style.zIndex = "9999";
+  
+      // Create the alert message text
+      var alertText = document.createTextNode("Your message sent successfully!!");
+      alertDiv.appendChild(alertText);
+  
+      // Add the alert div to the document body
+      document.body.appendChild(alertDiv);
+  
+      // Remove the alert after 2 seconds
+      setTimeout(function() {
+          alertDiv.remove();
+      }, 2000);
+  });
+  
+
+
+}
+const serviceID = "service_5gl7zse";
+  const templateID = "template_r1eiw29";
+
